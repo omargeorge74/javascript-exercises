24200bef88245e658e95b6d2ab9998faa94519a5
Omar George <79220589+omargeorge74@users.noreply.github.com>
2025-02-01 14:13:47 -0800

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let result = "";
+  if (num === 0) {
+    return "";
+  } else if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 1; i <= num; i++) {
+      result += string;
+    }
+  }
 
+  console.log(result);
+  return result;
 };
 
 // Do not edit below this line
