e85be0ebd2703a967cf2a72788ea3ded6c077979
Omar George <79220589+omargeorge74@users.noreply.github.com>
2025-02-01 14:53:08 -0800

    finished reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let result = "";
+  for (let i = 0; i <= string.length - 1; i++) {
+    let letter = string[i];
+    result = letter + result;
+  }
+  return result;
 };
 
 // Do not edit below this line
