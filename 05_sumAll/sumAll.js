6c6c82c667925095b5d75c6a13dcc6e6fc5731d7
Omar George <79220589+omargeorge74@users.noreply.github.com>
2025-02-04 21:59:41 -0800

    finished sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  let sum = 0;
+  if (first < 0 || second < 0) {
+    return "ERROR";
+  }
+  if (!Number.isInteger(first) || !Number.isInteger(second)) {
+    return "ERROR";
+  }
+  let min = Math.min(first, second);
+  let max = Math.max(first, second);
 
+  for (let i = min; i <= max; i++) {
+    sum = sum + i;
+  }
+
+  console.log(sum);
+  return sum;
 };
 
+sumAll(2.5, 4);
 // Do not edit below this line
 module.exports = sumAll;
