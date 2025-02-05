b20908dbed2d27fb6379a62e21198f376b9119a1
Omar George <79220589+omargeorge74@users.noreply.github.com>
2025-02-04 21:12:12 -0800

    finished remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (numbers, ...removeNumbers) {
+  return numbers.filter((num) => !removeNumbers.includes(num));
 };
+console.log(removeFromArray([1, 2, 3, 4], 3, 2, 1));
 
 // Do not edit below this line
 module.exports = removeFromArray;
