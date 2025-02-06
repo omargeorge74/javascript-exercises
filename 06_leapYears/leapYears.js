f791c3a2e91d9105ec14b0d1e79f26d0b92d6f30
Omar George <79220589+omargeorge74@users.noreply.github.com>
2025-02-05 18:05:18 -0800

    finished year of lerap[s

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (num) {
+  if (num % 4 === 0 && num % 400 === 0 && num % 100 === 0) {
+    return true;
+  } else if (num % 4 === 0 && num % 100 !== 0) {
+    return true;
+  } else if (num % 100 === 0) {
+    return false;
+  } else {
+    return false;
+  }
 };
-
+console.log(1900 % 400);
+console.log(leapYears(1996));
+console.log(leapYears(34992));
 // Do not edit below this line
 module.exports = leapYears;
